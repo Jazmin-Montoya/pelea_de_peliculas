6e7634e1aee6229bdf869d21d5faa48dcbd73fc6
Jazmin-Montoya <jazminmc07@gmail.com>
2025-03-31 18:24:02 -0700

    Agregue nueva funcionalidad index.js

--- index.js
+++ index.js
@@ -1,16 +1,113 @@
-const fetchData = async(searchTerm) =>{
+// const fetchData = async(searchTerm) =>{
+//     const response = await axios.get('http://omdbapi.com/',{
+//         params:{
+//             apikey:'93660f9e',
+//             s:'avengers'
+//         }
+//     })
+//     if(response.data.Error){
+//         return[]
+//     }
+//     console.log(response.data.Search)
+// }
+
+// fetchData()
+
+autoCompleteConfig={
+    renderOption(movie){
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+        return`
+            <img src="${imgSrc}"/>
+            ${movie.Title} (${movie.Year})
+        `
+    },
+    inputValue(movie){
+        return movie.Title
+    },
+    async fetchData(searchTerm){
+        apiMovieUrl = 'http://omdbapi.com/'
+        const response = await axios.get(apiMovieUrl,{
+            params:{
+                apikey:'93660f9e',
+                s: searchTerm
+            }
+        })
+        if (response.data.Error) {
+            return[]
+        }
+
+        console.log(response.data)
+        return response.data.Search
+    }
+}
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie,document.querySelector('#left-summery'), 'left')
+    }
+})
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie,document.querySelector('#right-summery'), 'right')
+    }
+})
+
+//Crear dos variables para leftMovie y rightMovie
+let leftMovie
+let rightMovie
+
+const onMovieSelect = async (movie,sumaryElement,side) =>{
     const response = await axios.get('http://omdbapi.com/',{
         params:{
             apikey:'93660f9e',
-            s:'avengers'
+            i:movie.imdbID
         }
     })
-    if(response.data.Error){
-        return[]
+    console.log(response.data)
+    sumaryElement.innerHTML=movieTemplate(response.data)
+
+    if(side === 'left'){
+        leftMovie = response.data
+    }else{
+        rightMovie = response.data
     }
-    console.log(response.data.Search)
+
+    //Preguntamos si tenemos amboslados
+    if(leftMovie && rightMovie){
+        //Entonces ejecutamos la funcion decomparacion
+        runComparison()
+    }
 }
-// fetchData()
+
+const runComparison = () => {
+    console.log('Comparacion de peliculas')
+    const leftSideStats = document.querySelectorAll('#left-summary .notification')
+    const rightSideStats = document.querySelectorAll('#right-summary .notification')
+
+    leftSideStats.forEach((leftStat, index)=>{
+        const rightStat = rightSideStats[index]
+        const leftSideValue= parseInt(leftStat.dataset.value)
+        const rightSideValue = parseInt(rightStat.dataset.value)
+
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary')
+            leftStat.classList.add('is-danger')
+        }else{
+            rightStat.classList.remove('is-primary')
+            rightStat.classList.add('is-danger')
+        }
+
+    })
+    
+}
+
 const root = document.querySelector('.autocomplete')
 root.innerHTML = `
     <label><b>Busqueda de Peliculas</b></label>
@@ -73,33 +170,33 @@ document.addEventListener('click', event => {
     }
 })
 
-const onMovieSelect = async(movie) =>{
-    const response=await axios.get('http://www.amdbapi,com/',{
-        params:{
-            apikey:'',
-            i:movie.imdbID
-        }
-    })
+// const OnMovieSelect = async(movie) =>{
+//     const response=await axios.get('http://www.amdbapi,com/',{
+//         params:{
+//             apikey:'',
+//             i:movie.imdbID
+//         }
+//     })
 
-    console.log(response.fetchData)
-    document.querySelector('#sumary').innerHTML=movieTemplate(response)
-}
+//     console.log(response.fetchData)
+//     document.querySelector('#sumary').innerHTML=movieTemplate(response)
+// }
 
-const movieTemplate=(movieDetail)=>{
-    return `
-        <article class="media">
-            <figure class "media-left">
-                <p class="image">
-                    <img src="${movieDetail.Poster}"/>
-                </p>
-            </figure>
-            <div class="media-constent">
-                <div class="content>
-                    <h1>${movieDetail.Title}</h1>
-                    <h4>${movieDetail.Genre}</h4>
-                    <p>${movieDetail.Plot}</p>
-                </div>
-            </div>
-        </article>
-    `
-}
+// const movieTemplate=(movieDetail)=>{
+//     return `
+//         <article class="media">
+//             <figure class "media-left">
+//                 <p class="image">
+//                     <img src="${movieDetail.Poster}"/>
+//                 </p>
+//             </figure>
+//             <div class="media-constent">
+//                 <div class="content>
+//                     <h1>${movieDetail.Title}</h1>
+//                     <h4>${movieDetail.Genre}</h4>
+//                     <p>${movieDetail.Plot}</p>
+//                 </div>
+//             </div>
+//         </article>
+//     `
+// }
